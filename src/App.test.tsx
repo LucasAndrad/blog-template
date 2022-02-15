a232f9bab92d93335c614272305764f93c2e6d14
Lucas Andrade <llucasandradd@gmail.com>
2022-02-15 19:49:37 -0300

    Add the first test for App.tsx home page component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('should show Home page text', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  expect(screen.getByRole('heading', { name: /home page/i })).toBeVisible();
 });
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const Home = () => {
   return (
-    <div>Home Page</div>
+    <h1>Home Page</h1>
   );
 };
