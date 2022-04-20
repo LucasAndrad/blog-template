a91df055ee2e0d448b7f7f2d8f0e07a06a9c3025
Lucas Andrade <llucasandradd@gmail.com>
2022-04-20 09:33:45 -0300

    Start to create usePokemons hook

---
+++ src/hooks/usePokemons.ts
@@ -0,0 +1,9 @@
+import { axios } from 'src/utils/axiosMiddleware';
+import { API_LINK, API_PATHS } from 'src/utils/constants';
+
+export const getPokemons = async () => {
+  const link = `${API_LINK}${API_PATHS.POKEMONS}`;
+  const response = await axios.get(link);
+
+  return response.data;
+};
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,9 @@ export const PATHS = {
   PROFILE: '/profile',
   POKEAPI: '/pokeapi',
 };
+
+export const API_LINK = 'https://pokeapi.co/api/v2';
+
+export const API_PATHS = {
+  POKEMONS: '/pokemon',
+};
