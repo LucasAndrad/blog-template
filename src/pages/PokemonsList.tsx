569831b3f72241d4806c484b49066173cc722ac4
Lucas Andrade <llucasandradd@gmail.com>
2022-04-20 09:50:18 -0300

    Add loading state at PokemonsList

--- src/pages/PokemonsList.tsx
+++ src/pages/PokemonsList.tsx
@@ -3,11 +3,14 @@ import { getPokemons } from 'src/hooks/usePokemons';
 
 export const PokemonsList = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const requestPokemons = async () => {
       const list = await getPokemons();
       setPokemons(list);
+      setLoading(false);
     };
     requestPokemons();
   }, []);
@@ -15,7 +18,10 @@ export const PokemonsList = () => {
   return (
     <div>
       <h2>Pokemons List</h2>
-      {pokemons.length &&
+      {loading &&
+        <p>Loading ...</p>
+      }
+      {pokemons.length > 0 &&
         pokemons.map((pokemon: any) => (
           <div key={JSON.stringify(pokemon)}>
             <p>{`${JSON.stringify(pokemon)}`}</p>
